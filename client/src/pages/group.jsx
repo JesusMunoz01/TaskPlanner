a6e1f83e84852afee4e587861941d4db35e3d08b
Jesus <jesus.munoz01@utrgv.edu>
2024-01-23 10:27:10 -0600

    added setup to invite users

--- client/src/pages/group.jsx
+++ client/src/pages/group.jsx
@@ -3,11 +3,13 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { useLocation, useParams } from "react-router-dom"
 import { Header } from "../components/header";
 import { SubmitForm } from "../components/submitForm";
+import { useState } from "react";
 
 export const Group = () => {
     const location = useLocation()
     const { from } = location.state
     const { groupID } = useParams();
+    const [invUsername, setUsername] = useState("")
     // console.log(from)
     // console.log(groupID)
 
@@ -25,20 +27,33 @@ export const Group = () => {
         }
     }
 
+    function showPrompt(){
+        let addBox = document.getElementById(`inviteUser`);
+            if(addBox.style.display === "none")
+                setTimeout(() => {addBox.style.display = "flex"}, "200")
+            else
+                addBox.style.display = "none";
+        }
+
     return <div className="group" id="group">
         <div className="header" onMouseDown={(e) => hidePrompt(e, "Collection")}>
-        <Header title={`${from.groupName}`} section="Collection" backArrow={"/groups"} 
+        <Header title={`${from.groupName}`} section="Collection" backArrow={"/groups"}
             newAction={ from.permissions === "Admin" ? 
             <>
             <button id="delGroup">Delete Group</button>
-            <input type="checkbox" id="groupUsers" style={{display: "none"}}/>
+            <input type="checkbox" id="groupUsers" onChange={showPrompt} style={{display: "none"}}/>
             <label id="groupUsers" htmlFor="groupUsers"><BsFillPersonLinesFill /></label>
             <div className="checkUsers">
-                <span id="groups-NoInvites">No Invites</span>
+                    <span id="groups-NoInvites">Members</span>
             </div>
             </>
             :
             null}/>
+            <div className="inviteUser" id="inviteUser" style={{display: "none"}}>
+                <label>Username:</label>
+                <input value={invUsername} onChange={(e) => setUsername(e.target.value)}></input>
+                <button id="inviteBtn" onClick={"test"}>Invite</button>
+            </div>
         </div>
         <div className="groupContent">
             <div className="groupContent" onMouseDown={(e) => hidePrompt(e, "Collection")}>
