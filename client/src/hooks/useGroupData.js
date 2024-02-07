d2988ab52f4ca15512bc05870346120a0cbb9688
Jesus <jesus.munoz01@utrgv.edu>
2024-02-06 22:56:23 -0600

    added template data for new group functions

---
+++ client/src/hooks/useGroupData.js
@@ -0,0 +1,17 @@
+
+export default function useGroupData() {
+
+    const editGroup = () => {
+        console.log("editGroup")
+    }
+
+    const deleteGroup = () => {
+        console.log("editGroup")
+    }
+
+    const leaveGroup = () => {
+        console.log("leaveGroup")
+    }
+
+    return {editGroup, deleteGroup, leaveGroup}
+};
