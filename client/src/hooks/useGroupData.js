274b2cc98b9593dd12a6f3d5e9e0ae621a858600
Jesus <jesus.munoz01@utrgv.edu>
2024-02-11 21:12:38 -0600

    added deleteGroup setup

--- client/src/hooks/useGroupData.js
+++ client/src/hooks/useGroupData.js
@@ -5,8 +5,25 @@ export default function useGroupData() {
         console.log("editGroup")
     }
 
-    const deleteGroup = () => {
-        console.log("editGroup")
+    const deleteGroup = async () => {
+        if(isUserLogged){
+            const userID = window.localStorage.getItem("userId");
+            const collectionID = currentCollection._id;
+            await fetch(`${__API__}/deleteCollection/${userID}/${collectionID}/tasks/${taskID}`, {
+                method: "DELETE", headers: {auth: check.access_token}, 
+            });
+            const deletedItem = collectionTasks.filter((task) => task._id !== taskID);
+            let collectionsData = fetchCollections();
+            collectionsData[currentCollectionIndex].tasks = deletedItem;
+            setCollectionTasks(deletedItem)
+            currentCollection.tasks = deletedItem;
+            setCurrentCollection(currentCollection);
+            filterTask(filterType, deletedItem);
+            collections.updateCollection(collectionsData)
+        }
+        else{
+            alert("You need to be logged in to perform this action")
+        }
     }
 
     const leaveGroup = () => {
