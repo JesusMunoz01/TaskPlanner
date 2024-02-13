ed7e1b8d835f2128ce5b783a7797e43d5014fd42
Jesus <jesus.munoz01@utrgv.edu>
2024-02-12 21:17:08 -0600

    made changes to edit group css and functionality

--- client/src/components/editGroupForm.jsx
+++ client/src/components/editGroupForm.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react"
+
 export default function EditGroupForm(params) {
     const [newGroupName, setNewGroupName] = useState("")
     const [newGroupDescription, setNewGroupDescription] = useState("")
     
     return (
-        <div className="editGroupForm">
-            <h2>Edit Group</h2>
-            <form>
-                <label htmlFor="groupName">Group Name: {}</label>
-                <input type="text" id="groupName" value={params.groupName} onChange={(e) => params.setGroupName(e.target.value)}></input>
-                <label htmlFor="groupDescription">Group Description</label>
-                <textarea id="groupDescription" value={params.groupDescription} onChange={(e) => params.setGroupDescription(e.target.value)}></textarea>
-                <label htmlFor="groupPermissions">Group Permissions</label>
-                <button id="saveGroup" onClick={params.confirmChanges}>Confirm</button>
-                <button id="deleteGroup" onClick={params.closeEdit}>Cacel</button>
-            </form>
+        <div className="editGroupFormBackground">
+            <div className="editGroupFormContainer">
+                <h1>Edit Group</h1>
+                <form className="editGroupForm">
+                    <label htmlFor="groupName">Old Group Name: {params.groupName}</label>
+                    <div style={{display: "flex", flexDirection: "row", fontSize: "1.2rem"}}>
+                        <label htmlFor="newGroupName">Group Name:</label>
+                        <input type="text" id="newGroupName" defaultValue={params.groupName} value={newGroupName} 
+                            onChange={(e) => setNewGroupName(e.target.value)}></input>
+                    </div>
+                    <label htmlFor="groupDescription">Old Group Description: {params.groupDescription}</label>
+                    <div style={{display: "flex", flexDirection: "row", fontSize: "1.2rem"}}>
+                        <label htmlFor="newGroupDescription">Group Description:</label>
+                        <textarea id="newGroupDescription" defaultValue={params.groupDescription} value={newGroupDescription} 
+                            onChange={(e) => setNewGroupDescription(e.target.value)}></textarea>
+                    </div>
+                    <div className="editGroupFormActions">
+                        <button id="saveGroup" onClick={params.confirmChanges}>Confirm</button>
+                        <button id="deleteGroup" onClick={params.closeEdit}>Cancel</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 };
