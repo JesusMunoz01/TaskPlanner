ada9e5422e79783ef1e59b8bd1badd6642d4f85a
Jesus <jesus.munoz01@utrgv.edu>
2024-02-14 15:43:31 -0600

    added template tests for group page

--- client/src/tests/group.test.jsx
+++ client/src/tests/group.test.jsx
@@ -19,11 +19,101 @@ describe('Tests for the groups Page', () => {
         expect(text).toBeInTheDocument()
     })
 
+    test.skip('Testing for admin features (Delete, Edit, and invite buttons)', () => {
+        const mockData = {invites: ["testGroup"], joined: [{_id: 1, groupName: 'Test Group', groupDescription: 'Test Description', permissions: 'Admin'},
+        {_id: 2, groupName: 'Test Group 2', groupDescription: 'Test Description 2', permissions: 'Member'}]}
+        const renderedGroup = render(
+            <UserContext.Provider value={{groupData: mockData, setGroupData: () => {}}}>
+                <MemoryRouter>
+                    <Groups userData={mockData} isLogged={true}/>
+                    <Routes>
+                        <Route path='/' element={null}/>    
+                        <Route path="/groups/:groupId" element={<div>Group Page</div>}/>
+                    </Routes>
+                </MemoryRouter>
+            </UserContext.Provider>)
+    
+            const groupText = renderedGroup.getByLabelText('groupTitle1')
+            const invText = renderedGroup.getByLabelText('inviteTitletestGroup')
+    
+            expect(groupText).toBeInTheDocument()
+            expect(invText).toBeInTheDocument()
+    })
+
+    test.skip('Test editing a group', () => {
+        
+    })
+
+    test.skip('Test deleting a group', () => {
+        
+    })
+
+    test.skip('Test leaving a group', () => {
+        
+    })
+
+    test.skip('Test updating a group', () => {
+
+    })
+
+    test.skip('Test inviting a user to a group', () => {
+            
+    })
+
+    test.skip('Test creating a group collection', () => {
+            
+    })
+
+    test.skip('Test creating a group collection', () => {
+            
+    })
+
+    test.skip('Test editing a group collection', () => {
+            
+    })
+
+    test.skip('Test deleting a group collection', () => {
+            
+    })
+
 })
 
 describe('Tests for the group Page API ', () => {
             
-    test.skip('Test creating a collection', async () => {
+    test.skip('Test API for editing a group', () => {
+        
+    })
+
+    test.skip('Test API for deleting a group', () => {
+        
+    })
+
+    test.skip('Test API for leaving a group', () => {
+        
+    })
+
+    test.skip('Test API for updating a group', () => {
+
+    })
+
+    test.skip('Test API for inviting a user to a group', () => {
+            
+    })
+
+    test.skip('Test API for creating a group collection', () => {
+            
+    })
+
+    test.skip('Test API for creating a group collection', () => {
+            
+    })
+
+    test.skip('Test API for editing a group collection', () => {
+            
+    })
+
+    test.skip('Test API for deleting a group collection', () => {
+            
     })
 
 })
