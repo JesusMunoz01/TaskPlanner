04305b7a34117225310e0d864584613e0cb1a26f
Jesus <jesus.munoz01@utrgv.edu>
2024-02-21 22:36:06 -0600

    added setGroupData to update data function

--- client/src/pages/groupCollectionTasks.jsx
+++ client/src/pages/groupCollectionTasks.jsx
@@ -160,8 +160,13 @@ export const GroupCollectionTasks = ({isUserLogged}) => {
                 collectionsData[currentCollectionIndex] = updatedValues;
                 setCollectionTasks(updatedValues.tasks)
                 setCurrentCollection(updatedValues);
+                setGroupData((prev) => {
+                    return {
+                        invites: prev.invites,
+                        joined: collectionsData
+                    }
+                })
                 filterTask(filterType, updatedValues.tasks)
-                collections.updateCollection(collectionsData);
             }catch(error){
                 console.log(error)
             }
