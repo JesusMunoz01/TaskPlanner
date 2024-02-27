69ae181775c21922f1ada32fd379ea953d510cab
Jesus <jesus.munoz01@utrgv.edu>
2024-02-27 14:41:49 -0600

    created a component for error routes

---
+++ client/src/components/errorRoute.jsx
@@ -0,0 +1,7 @@
+export default function ErrorRoute() {
+    return (
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h1>404: Page Not Found</h1>
+        </div>
+    );
+}
