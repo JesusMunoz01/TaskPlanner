eccc3418dffc42a8b49ec944c7d89f6af6356588
Jesus <jesus.munoz01@utrgv.edu>
2024-02-28 13:16:36 -0600

    added a timer to errorRoute redirect

--- client/src/components/errorRoute.jsx
+++ client/src/components/errorRoute.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function ErrorRoute() {
+    const [timer, setTimer] = useState(5);
+    
+    useEffect(() => {
+        if(timer === 0) return redirect();
+        const interval = setInterval(() => {
+            setTimer(timer => timer - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [timer]);
 
     const redirect = () => {
         window.location.href = "/landing";
     }
 
     return (
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
             <h1>404: Page Not Found</h1>
             <h2>Redirecting to landing page...</h2>
-            {setTimeout(redirect, 3000)}
+            <h2>in {timer} seconds</h2>
         </div>
     );
 }
