dcc332ebf204a5db2df8ae11d175554b6ef551af
Jesus <jesus.munoz01@utrgv.edu>
2024-03-05 18:36:07 -0600

    template tests

---
+++ client/src/tests/landing.test.jsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render, cleanup, act } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import { Landing } from '../pages/landing'
+
+describe('Landing Page', () => {
+
+    test.skip('Testing component display', async () => {
+        
+    })
+
+    test.skip('Testing Tasks Link', async () => {
+            
+    })
+
+    test.skip('Testing Collections Link', async () => {
+            
+    })
+
+    test.skip('Testing Groups Link', async () => {
+            
+    })
+
+})
